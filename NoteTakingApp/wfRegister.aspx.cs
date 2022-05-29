c74fad6a2427b3f9e779af86975a251ac15d8004
Asaf Senyor <Asaf.senyor@outlook.co.il>
2022-05-29 19:36:04 +0300

    added  the back side of register

--- NoteTakingApp/wfRegister.aspx.cs
+++ NoteTakingApp/wfRegister.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 namespace NoteTakingApp
 {
@@ -16,7 +17,38 @@ namespace NoteTakingApp
 
         protected void accept_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                string selectQuery = "SELECT userID, name, auth FROM tUsers WHERE emailAddress='" + tbxEmailAdress.Text + "' or password='" + tbxPassword.Text + "'";
+                DataTable user = MyAdoHelper.ExecuteDataTable("SAF.mdf", selectQuery);
+                int rows = user.Rows.Count;
+                if (rows == 1)
+                {
+                    lblResult.Text = "a user with this account has already registered!";
+                    return;
+                }
+                if(rows == 0)
+                {
+                    string insertNonQuery = "INSERT INTO tUsers(name, emailAddress, password, auth) values('"+tbxUserNAme.Text+"', '"+tbxEmailAdress+"', '"+tbxPassword.Text+"', 1) ";
+                    int result = MyAdoHelper.RowsAffected("NoteTaking.aspx", insertNonQuery);
+                    if(result == 1)
+                    {
+                        Response.Redirect("wfSuccess.aspx");
+                    }
+                    if(result > 1)
+                    {
+                        lblResult.Text = "an unexpected problem has occurred, please speak to the manager.";
+                    }
+                    if(result == 0)
+                    {
+                        lblResult.Text = "user isn't registered.";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return;
+            }
         }
     }
 }
---
+++ NoteTakingApp/wfSuccess.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/NoteTaking.Master" AutoEventWireup="true" CodeBehind="wfSuccess.aspx.cs" Inherits="NoteTakingApp.wfSuccess" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>you made it!</p>
+</asp:Content>
